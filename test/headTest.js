7f0ff2bf5d5c4859aa46eba06e6e219bb9fb2ae4
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-23 20:39:08 +0000

    refactored headTest.js and added mocha and chai with frank

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,20 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head(5, 6, 9), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 5 for [5, 6, 9]", () => {
+    assert.strictEqual(head([5, 6, 9]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns one element if only one element inputted", () => {
+    assert.strictEqual(head([5]),5);
+  });
+
+  it("returns undefined if array is empty", () => {
+    assert.strictEqual(head(), undefined);
+  });
+});
