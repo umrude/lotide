538c56d9467bce0032ee62daec9953314732fc6e
Gabi Geollegue <gabigeollegue@gmail.com>
2019-09-23 21:07:35 +0000

    refactored tail.js and tailTest.js with frank

--- tail.js
+++ tail.js
@@ -1,14 +1,12 @@
 
 let tail = function(arr) {
   let newArr = [];
-  if (arr === undefined) {
-    return arr;
-  } else if (arr.length > 1) {
+  if (arr.length > 1) {
     for (let i = 1; i <= arr.length - 1; i++) {
       newArr.push(arr[i]);
     } return newArr;
   } else {
-    return [];
+    return undefined;
   }
 };
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,14 @@
-const assertEqual = require('../assertEqual');
 const tail = require("../tail");
+const assert = require('chai').assert;
 
-//const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("should return [4, 5, 6] from [1, 2, 3, 4, 5, 6] ", () => {
+    let input = [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(tail(input), [2, 3, 4, 5, 6]);
+  });
+  it("The tail of ['hi'] is undefined", () => {
+    let input = ["hi"];
+    assert.deepEqual(tail(input), undefined);
+  });
+});
